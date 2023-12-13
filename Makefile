155a08cdb8f5b77dedebeb690482b6434fd80e13
Andrew Zolensky <andrewlzolensky@gmail.com>
2023-12-13 17:59:08 -0500

    Second Commit

--- Makefile
+++ Makefile
@@ -1,13 +1,15 @@
+CFLAGS = -g -Wall
+
 all: app
 
 app: app.o customer.o
-	cc app.o customer.o -o app
+	cc $(CFLAGS) app.o customer.o -o app
 
 app.o: app.c customer.h
-	cc -c app.c -o app.o
+	cc $(CFLAGS) -c app.c -o app.o
 
 customer.o: customer.c customer.h
-	cc -c customer.c -o customer.o
+	cc $(CFLAGS) -c customer.c -o customer.o
 
 clean:
 	rm -f app.o customer.o app
--- app.c
+++ app.c
@@ -9,5 +9,20 @@ int main() {
     customer* my_table [BUCKETS];
     char name [4]; int id, bucket;
 
+    while(1) {
+        printf("Enter a customer name and id\n");
+        scanf("%s %d", name, &id);
+        bucket = id % BUCKETS;
+        add_customer(&my_table[bucket], id, name);
+    }
+
+    for (int i = 0; i < BUCKETS; i++) {
+        print_list(&my_table[i]);
+    }
+
+    for (int i = 0; i < BUCKETS; i++) {
+        delete_list(&my_table[i]);
+    }
+
     return(0);
 }
